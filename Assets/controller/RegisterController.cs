6057dcbdb63aaec2895ad9636740c78e2c0070d0
gellirosario <gellirosario@gmail.com>
2019-10-19 17:27:41 +0800

    Added scene switcher script

--- Assets/controller/RegisterController.cs
+++ Assets/controller/RegisterController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LogInController : MonoBehaviour
+public class RegisterController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/scripts/SceneSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void LoadNextScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
