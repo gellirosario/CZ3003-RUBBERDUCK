d7d643982796bea98ca4eb7df325a37438672775
Edmund Ang <edmund_a@hotmail.com>
2019-10-20 00:58:20 +0800

    Added in firebase AUTH Register function work Login Function: not working, go to issue

--- Assets/controller/LogInController.cs
+++ Assets/controller/LogInController.cs
@@ -1,18 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
 
 public class LogInController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update    
+    private FirebaseAuth auth;
+
+
+    public InputField EmailInputField, PasswordInputField;
+    public Button LoginButton;
+
+
+    public Text ErrorText;
+
     void Start()
     {
+        auth = FirebaseAuth.DefaultInstance;
+        //Just an example to save typing in the login form
+       
+
         
+        LoginButton.onClick.AddListener(() => Login(EmailInputField.text.Trim(), PasswordInputField.text.Trim()));
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void UpdateErrorMessage(string message)
     {
-        
+        ErrorText.text = message;
+        Invoke("ClearErrorMessage", 3);
     }
+
+
+
+
+    public void Login(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync error: " + task.Exception);
+                if (task.Exception.InnerExceptions.Count > 0)
+                    UpdateErrorMessage(task.Exception.InnerExceptions[0].Message);
+                return;
+            }
+           
+              FirebaseUser user = task.Result;
+             Debug.LogFormat("User signed in successfully: {0} ({1})",
+                user.DisplayName, user.UserId);
+
+            PlayerPrefs.SetString("LoginUser", user != null ? user.Email : "Unknown");
+            SceneManager.LoadScene("play");
+
+        });
+    }
+
+
+
+
 }
+
--- Assets/controller/RegisterController.cs
+++ Assets/controller/RegisterController.cs
@@ -1,18 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
 
 public class RegisterController : MonoBehaviour
 {
+    private FirebaseAuth auth;
+    public InputField UserNameInput, PasswordInput;
+    public Button RegisterButton;
+    public Text ErrorText;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        auth = FirebaseAuth.DefaultInstance;
+
         
+
+        RegisterButton.onClick.AddListener(() => Signup(UserNameInput.text, PasswordInput.text));
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void UpdateErrorMessage(string message)
     {
-        
+        ErrorText.text = message;
+        Invoke("ClearErrorMessage", 3);
     }
+
+    void ClearErrorMessage()
+    {
+        ErrorText.text = "";
+    }
+
+    public void Signup(string email, string password)
+    {
+
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            //Error handling
+            return;
+        }
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync error: " + task.Exception);
+                if (task.Exception.InnerExceptions.Count > 0)
+                    UpdateErrorMessage(task.Exception.InnerExceptions[0].Message);
+                return;
+            }
+
+            FirebaseUser newUser = task.Result; // Firebase user has been created.
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+            UpdateErrorMessage("Signup Success");
+
+            SceneManager.LoadScene("play");
+        });
+    }
+
+
+    // Update is called once per frame
+    
 }
