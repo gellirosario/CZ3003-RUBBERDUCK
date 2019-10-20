349fee0546193a43430f6d14c5d1d3651c425c7f
gellirosario <gellirosario@gmail.com>
2019-10-20 18:04:40 +0800

    Updated question class

--- Assets/controller/LevelController.cs
+++ Assets/controller/LevelController.cs
@@ -1,15 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class LevelController : MonoBehaviour
 {
-    //public List<Question> questionList = new List<>();
-    private Question[] questions = null;
-    public Question[] Questions { get { return questions; } }
+    private List<Question> questionList = new List<Question>();
+    private Firebase.FirebaseApp app;
+    private bool isFirebaseInitialized = false;
+    private DatabaseReference reference;
+    
+    public void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                InitializeFirebase();
+                
+            } else {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+    
+    // Initialize the Firebase database
+    
+    protected virtual void InitializeFirebase() {
+        
+        app.SetEditorDatabaseUrl("https://teamrubberduck-1420e.firebaseio.com/");
+        
+        if (app.Options.DatabaseUrl != null)
+            app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+        UnityEngine.Debug.LogError("Test");
+        writeTestQuestionPool();
+        isFirebaseInitialized = true;
+    }
 
-    private List<int> FinishedQuestions = new List<int>();
-    private int currentQuestion = 0;
-    private int score;
+    // Testing purposes
+    private void writeTestQuestionPool() {
+        
+        //for (int i = 0; i < 10; i++)
+        //{
+            AddQuestionToDatabase("1");
+        //}
+        
+        
+    }
 
+    private void AddQuestionToDatabase(string questionID)
+    {
+        string difficulty;
+        if (int.Parse(questionID) % 2 == 0)
+        {
+            difficulty = "Medium";
+        }
+        else
+        {
+            difficulty = "Easy";
+        }
+        
+        Question question = new Question(1,1,difficulty,"Test Question " + questionID,1,"1","2","3","4");
+        questionList.Add(question);
+        string json = JsonUtility.ToJson(question);
+
+        FirebaseDatabase.DefaultInstance.RootReference.Child("questions").Child(questionID).SetValueAsync(json);
+    }
 }
--- Assets/model/Question.cs
+++ Assets/model/Question.cs
@@ -1,15 +1,44 @@
 ﻿[System.Serializable]
 public class Question
 {
-    public enum Difficulty { Easy, Medium, Hard }
-
     public int world;
     public int stage;
-    public Difficulty difficulty;
+    public string difficulty;
     public string question;
     public int answer;
     public string option1;
     public string option2;
     public string option3;
     public string option4;
+    
+    public Question() {
+    }
+
+    public Question(int world, int stage, string difficulty, string question, int answer, string o1, string o2, string o3, string o4) {
+        this.world = world;
+        this.stage = stage;
+        this.difficulty = difficulty;
+        this.question = question;
+        this.answer = answer;
+        this.option1 = o1;
+        this.option2 = o2;
+        this.option3 = o3;
+        this.option4 = o4;
+    }
+    /*
+    public Dictionary<string, Object> ToDictionary() {
+        Dictionary<string, Object> result = new Dictionary<string, Object>();
+        
+        result["world"] = world;
+        result["stage"] = stage;
+        result["difficulty"] = difficulty;
+        result["question"] = question;
+        result["answer"] = answer;
+        result["option1"] = option1;
+        result["option2"] = option2;
+        result["option3"] = option3;
+        result["option4"] = option4;
+        
+        return result;
+    }*/
 }
