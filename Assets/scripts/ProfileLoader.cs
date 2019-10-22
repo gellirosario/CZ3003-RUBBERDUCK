ed2b3983302ddb4016bc9a32761ac04c53424405
mseetbh <mseetbh@gmail.com>
2019-10-23 00:47:31 +0800

    Profile Loader - Work in progress

---
+++ Assets/scripts/ProfileLoader.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class ProfileLoader : MonoBehaviour
+{
+    public static ProfileLoader Instance { get; set; }
+
+    private FirebaseApp app;
+    private DatabaseReference reference;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://teamrubberduck-1420e.firebaseio.com/");
+                reference = FirebaseDatabase.DefaultInstance.RootReference;
+                LoadUserData();
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+
+    private void LoadUserData()
+    {
+        string uid = PlayerPrefs.GetString("UserID");
+        Debug.Log(uid);
+
+        FirebaseDatabase.DefaultInstance.GetReference("Users").GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+
+            }
+            else if (task.IsCompleted)
+            {
+
+                DataSnapshot snapshot = task.Result;
+                //to do the actual loading
+
+            }
+
+        });
+
+      
+    }
+}
