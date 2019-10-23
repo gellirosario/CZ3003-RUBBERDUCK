313f7ae3f02718525c56acfc56ce8590d4fbebb4
heemyew <49015870+heemyew@users.noreply.github.com>
2019-10-23 23:14:08 +0800

    customize function done
    able to change firebase characterID base on what user choose, 0 for red 1 for black

--- Assets/controller/CustomizeController.cs
+++ Assets/controller/CustomizeController.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class CustomizeController : MonoBehaviour
 {
+    private FirebaseApp app;
+    private DatabaseReference reference;
+    private bool isFirebaseInitialized = false;
+    //private string userId;
+    public Text messageTxt;
     private int selectIndex;
     [Header("UI reference")]
     [SerializeField] private Image characterSplash;
@@ -42,12 +50,41 @@ public class CustomizeController : MonoBehaviour
     public void Confirm()
     {
         Debug.Log(string.Format("selected {0}:{1} ",selectIndex,characterList[selectIndex].characterName));
+        reference.Child("Player").Child(PlayerPrefs.GetString("UserID")).Child("characterID").SetValueAsync(selectIndex);
+        messageTxt.text = "Character change to "+characterList[selectIndex].characterName;
     }
     // Start is called before the first frame update
     private void Start()
     {
+        messageTxt.text = "";
         UpdateCharacterSelectionUI();
         Debug.Log(string.Format("start"));
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://teamrubberduck-1420e.firebaseio.com/");
+                reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+        /*Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            //string name = user.DisplayName;
+            //string email = user.Email;
+            //System.Uri photo_url = user.PhotoUrl;
+            // The user's Id, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server, if you
+            // have one; use User.TokenAsync() instead.
+            userId = user.UserId;
+        }*/
     }
 
     // Update is called once per frame
