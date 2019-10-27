1e5cfcd6781a7680f2351bf1bf9625fa3dc7bde4
gellirosario <gellirosario@gmail.com>
2019-10-27 23:18:41 +0800

    Changed qn logic

---
+++ Assets/controller/CodeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class CodeController : MonoBehaviour
+{
+    private FirebaseApp app;
+    private DatabaseReference reference;
+    private bool isFirebaseInitialized = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
