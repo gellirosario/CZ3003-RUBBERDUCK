ba5af674a0a8314acc46c2de2c00194b2882f02a
xKwan <clow025@e.ntu.edu.sg>
2019-10-28 23:45:46 +0800

    Prefab buttons

---
+++ Assets/scripts/AssignmentButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssignmentButton : MonoBehaviour
+{
+    public Button button;
+    public Text assignmentLabel;
+
+    private Item item;
+    private AssignmentScrollList scrollList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Setup(Item currentItem, AssignmentScrollList currentScrollList)
+    {
+        item = currentItem;
+        assignmentLabel.text = item.assignmentName;
+        
+        scrollList = currentScrollList;
+
+    }
+
+
+}
---
+++ Assets/scripts/AssignmentScrollList.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item
+{
+    public string assignmentName;
+}
+
+public class AssignmentScrollList : MonoBehaviour
+{
+
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public AssignmentScrollList createAssignment;
+    public SimpleObjectPool buttonObjectPool;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        RefreshDisplay();
+    }
+
+    void RefreshDisplay()
+    {
+        //RemoveButtons();
+        AddButtons();
+    }
+
+    /*private void RemoveButtons()
+    {
+        while (contentPanel.childCount > 0)
+        {
+            GameObject toRemove = transform.GetChild(0).gameObject;
+            buttonObjectPool.ReturnObject(toRemove);
+        }
+    }*/
+
+    private void AddButtons()
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            Item item = itemList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+            newButton.transform.SetParent(contentPanel);
+
+            AssignmentButton assignmentButton = newButton.GetComponent<AssignmentButton>();
+            assignmentButton.Setup(item, this);
+        }
+    }
+    
+    /*public void addAssignment(Item item)
+    {
+        if (otherShop.gold >= item.price)
+        {
+            gold += item.price;
+            otherShop.gold -= item.price;
+
+            AddItem(item, otherShop);
+            RemoveItem(item, this);
+
+            RefreshDisplay();
+            otherShop.RefreshDisplay();
+            Debug.Log("enough gold");
+
+        }
+        Debug.Log("attempted");
+    }*/
+    
+    void AddItem(Item itemToAdd, AssignmentScrollList assignmentList)
+    {
+        assignmentList.itemList.Add(itemToAdd);
+    }
+
+    private void RemoveItem(Item itemToRemove, AssignmentScrollList assignmentList)
+    {
+        for (int i = assignmentList.itemList.Count - 1; i >= 0; i--)
+        {
+            if (assignmentList.itemList[i] == itemToRemove)
+            {
+                assignmentList.itemList.RemoveAt(i);
+            }
+        }
+    }
+}
