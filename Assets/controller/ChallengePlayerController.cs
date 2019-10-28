e5c3bee4ffb719eef4baa7a14e5b74c4a8634dc5
heemyew <49015870+heemyew@users.noreply.github.com>
2019-10-28 23:46:22 +0800

    challengeplayer-d
    able to search firebase for the username, if match send uid as userpref to the challengeselectpage

--- Assets/controller/ChallengePlayerController.cs
+++ Assets/controller/ChallengePlayerController.cs
@@ -6,6 +6,7 @@ using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
 using UnityEngine.SceneManagement;
+using Firebase.Extensions;
 
 public class ChallengePlayerController : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class ChallengePlayerController : MonoBehaviour
     public InputField inputField;
     private bool isFirebaseInitialized = false;
     public Text messageTxt;
+    private string uid = null;
     private bool selected = false;
     //public InputField playerInput;
     // Start is called before the first frame update
@@ -38,13 +40,14 @@ public class ChallengePlayerController : MonoBehaviour
     }
     public void Challenge()
     {
+        
         Debug.Log(inputField.text.Trim());
         //Debug.Log(string.Format("selected {0}:{1} ", selectIndex, characterList[selectIndex].characterName));
         //reference.Child("Player").Child(PlayerPrefs.GetString("UserID")).Child("characterID").SetValueAsync(selectIndex);
         //messageTxt.text = "Character change to " + characterList[selectIndex].characterName;
         //reference.Child("Users").Child("name").SetValueAsync(inputField.text.Trim());
 
-        FirebaseDatabase.DefaultInstance.GetReference("Users").GetValueAsync().ContinueWith(task => 
+        FirebaseDatabase.DefaultInstance.GetReference("Users").GetValueAsync().ContinueWithOnMainThread(task => 
         {
          if (task.IsFaulted)
          {
@@ -54,36 +57,58 @@ public class ChallengePlayerController : MonoBehaviour
          {
                  DataSnapshot snapshot = task.Result;
                 //Debug.Log(snapshot.Child("name").Value.ToString()+"yay");
-                /*foreach (DataSnapshot user in snapshot.Children)
+                foreach (DataSnapshot user in snapshot.Children)
                 {
-                    //IDictionary dictUser = (IDictionary)user.Value ;
-                    IDictionary dictUser = (IDictionary)user.Value;
-
-                   if(dictUser["name"].ToString() == inputField.text.ToString())
-                   {
-                        //string uid = user.Key.ToString();
-                        //Debug.Log("" + dictUser["name"] + " - " + dictUser["email"]+ user.Key);
-                        //PlayerPrefs.SetString("challengedID", uid);
-                        selected = true;
-                        //SceneManager.LoadScene("ChallengeSelect");
-                        //break;
-
-                    }
-
-                }*/
-               if(selected)
-                {
-                    Debug.Log(" found");
                     
+                    //IDictionary dictUser = (IDictionary)user.Value;
+                    var dictUser = (IDictionary<string, object>)user.Value;
+                    //Question quest = new Question(dictUser);
+                    //questionList_All.Add(quest);
+                    //Debug.Log(user.Key);
+                    
+                    if (dictUser["name"].ToString() == inputField.text.ToString())
+                   {
+                         uid = user.Key.ToString();
+                        //Debug.Log("" + dictUser["name"] + " - " + dictUser["email"]+ user.Key);
+                        //Debug.Log(uid);
+                        PlayerPrefs.SetString("challengedID", uid);
+                        selected = true;
+                        Debug.Log(" found");
+                        //break;
+                    }
+                    Debug.Log(" loop");
                 }
-                else { messageTxt.text = "Cannot find user " + inputField.text + "in firebase"; }
+                Debug.Log("exit loop");
+                if(uid!=null)
+                {
+                    Debug.Log(uid);
+                    userfound();
+                }
+                /*if (selected)
+                {
+                    //Debug.Log(" found");
+                    
+                    Debug.Log(" found2");
+                    //elected = false;
+                }*/
+                else 
+                {
+                    Debug.Log(" ntfound");
+                    messageTxt.text = "Cannot find user " + inputField.text + " in firebase"; 
+                }
                
                 
 
             }
      });
     }
+    private void userfound()
+    {
+        Debug.Log(" found2");
+        uid = null;
+        SceneManager.LoadScene("ChallengeSelect");
 
+    }
     // Update is called once per frame
     void Update()
     {
