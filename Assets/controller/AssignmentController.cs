cd9782488970222dd719acfa219615f0f5282de1
jngiam004 <49016056+jngiam004@users.noreply.github.com>
2019-10-30 21:49:10 +0800

    create assignment

---
+++ Assets/controller/AssignmentController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+public class AssignmentController : MonoBehaviour
+{
+    public InputField assignmentInput;
+    public static string assignmentName;
+    private List<Question> questionList = new List<Question>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Retrieve Question List According to World and Stage
+        print("Before loading questionlist count: " + questionList.Count);
+        if (questionList.Count == 0)
+        {
+            questionList = QuestionLoader.Instance.FilterQuestionsByWorldAndStage(1,1);
+            print("After loading questionlist count: " + questionList.Count);
+        }
+    }
+
+    public void SaveAssignmentToDatabase()
+    {
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://teamrubberduck-1420e.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        //Assignment assignment = new Assignment(assignmentInput.text,0,0,0,"1","1",0,"1", "1", "1", "1");
+        //string json = JsonUtility.ToJson(assignment);
+
+        //reference.Child("Assignment").SetRawJsonValueAsync(json);
+        print("After loading questionlist count: " + questionList.Count);
+        for (int i = 0; i < questionList.Count; i++)
+        {
+            Assignment assignment = new Assignment(assignmentInput.text, questionList[i].qnID, questionList[i].world, questionList[i].stage, questionList[i].difficulty,
+                questionList[i].question, questionList[i].answer, questionList[i].option1, questionList[i].option2, questionList[i].option3, questionList[i].option4);
+            string json = JsonUtility.ToJson(assignment);
+            reference.Child("Assignment").Child(PlayerPrefs.GetString("UserID")).SetRawJsonValueAsync(json);
+        }
+        /*Assignment assignment = new Assignment(assignmentInput.text, 1, 2, 3, "1",
+                "1", 100, "1", "1", "1", "1");*/
+        //SetValueAsync(int/str) accept interger or string data type
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/model/Assignment.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Assignment 
+{
+    public string assignmentName;
+    public int qnID;
+    public int world;
+    public int stage;
+    public string difficulty;
+    public string question;
+    public int answer;
+    public string option1;
+    public string option2;
+    public string option3;
+    public string option4;
+
+    public Assignment()
+    {
+    }
+
+    public Assignment(string assignmentName, int qnID, int world, int stage, string difficulty, string question, int answer, string o1, string o2, string o3, string o4)
+    {
+        this.assignmentName = assignmentName;
+        this.qnID = qnID;
+        this.world = world;
+        this.stage = stage;
+        this.difficulty = difficulty;
+        this.question = question;
+        this.answer = answer;
+        this.option1 = o1;
+        this.option2 = o2;
+        this.option3 = o3;
+        this.option4 = o4;
+    }
+
+    // Updating IDictionary to Class
+    public Assignment(IDictionary<string, object> dict)
+    {
+        this.assignmentName = dict["assignmentName"].ToString();
+        this.qnID = int.Parse(dict["qnID"].ToString());
+        this.world = int.Parse(dict["world"].ToString());
+        this.stage = int.Parse(dict["stage"].ToString());
+        this.difficulty = dict["difficulty"].ToString();
+        this.question = dict["question"].ToString();
+        this.answer = int.Parse(dict["answer"].ToString());
+        this.option1 = dict["option1"].ToString();
+        this.option2 = dict["option2"].ToString();
+        this.option3 = dict["option3"].ToString();
+        this.option4 = dict["option4"].ToString();
+    }
+}
