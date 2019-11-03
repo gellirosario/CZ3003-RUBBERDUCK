c50f133404042d7af8b313667117e7a4dab8570f
jngiam004 <49016056+jngiam004@users.noreply.github.com>
2019-11-04 00:21:17 +0800

    Fixing the font size of assignment questions

--- Assets/scripts/AssignmentQuestionContainer.cs
+++ Assets/scripts/AssignmentQuestionContainer.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class AssignmentQuestionContainer : MonoBehaviour
 {
+    //public Texture newTexture;
+    public RawImage newImage;
+
     public Text assignmentLabel;
     public Text worldText;
     public Text stageText;
@@ -27,16 +30,16 @@ public class AssignmentQuestionContainer : MonoBehaviour
         questionItem = currentItem;
         scrollList = currentScrollList;
 
-        assignmentLabel.text = questionItem.assignmentTxt;
+        /*assignmentLabel.text = questionItem.assignmentTxt;
         worldText.text = questionItem.worldTxt;
         stageText.text = questionItem.stageTxt;
         difficultyText.text = questionItem.difficultyTxt;
-        questionIDText.text = questionItem.questionIDTxt;
+        questionIDText.text = questionItem.questionIDTxt;*/
         questionText.text = questionItem.questionTxt;
         o1Text.text = questionItem.o1Txt;
         o2Text.text = questionItem.o2Txt;
         o3Text.text = questionItem.o3Txt;
         o4Text.text = questionItem.o4Txt;
-        answerText.text = questionItem.answerTxt;
+        //sanswerText.text = questionItem.answerTxt;
     }
 }
