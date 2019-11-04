2ab449865615f7ce82425cdfb3ac73c063b247f1
mseetbh <mseetbh@gmail.com>
2019-11-04 20:36:39 +0800

    changed userId to username

--- Assets/model/Challenge.cs
+++ Assets/model/Challenge.cs
@@ -20,7 +20,7 @@ public class ChallengeEntry //this class is for storing player ids that have com
 public class Challenge
 {
     public string challengeId;
-    public string userId;
+    public string username;
     public List<int> challengeQns; //List of qn ids in the challenge
     public List<ChallengeEntry> challengePlayers;
 
@@ -33,7 +33,7 @@ public class Challenge
     {
         this.challengeId = generateId();
         this.challengeQns = qns;
-        this.userId = creator;
+        this.username = creator;
         this.challengePlayers = new List<ChallengeEntry>();
     }
 
@@ -41,7 +41,7 @@ public class Challenge
     {
         this.challengeId = id;
         this.challengeQns = qns;
-        this.userId = creator;
+        this.username = creator;
         this.challengePlayers = players;
     }
 
