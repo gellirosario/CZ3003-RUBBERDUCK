26971731fe038a9c8e9a1d9d029fe7011478b613
gellirosario <gellirosario@gmail.com>
2019-11-06 00:53:09 +0800

    Relink

---
+++ Assets/Tests/PerformanceTestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.PerformanceTesting;
+
+namespace Tests
+{
+    public class PerformanceTestScript
+    {
+        /*
+        [UnityTest, Performance]
+        public IEnumerator RenderPerformance()
+        {
+            yield return SceneManager.LoadSceneAsync("PlayerMain", LoadSceneMode.Additive);
+
+            SetActiveScene("PlayerMain");
+
+            // Instantiate performance test object in scene
+            var renderPerformanceTest =
+                SetupPerfTest<DynamicRenderPerformanceMonoBehaviourTest>();
+
+            // allow time to settle before taking measurements
+            yield return new WaitForSecondsRealtime(SettleTime);
+
+            // use ProfilerMarkers API from Performance Test Extension
+            using (Measure.ProfilerMarkers(SamplerNames))
+            {
+
+                // Set CaptureMetrics flag to TRUE; 
+                // let's start capturing metrics
+                renderPerformanceTest.component.CaptureMetrics = true;
+
+                // Run the MonoBehaviour Test
+                yield return renderPerformanceTest;
+            }
+
+            yield return SceneManager.UnloadSceneAsync(spiralSceneName);
+        }*/
+    }
+}
