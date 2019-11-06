ef9b92a04c8dbcc8d4b7839b559515c134d4bbaa
mseetbh <mseetbh@gmail.com>
2019-11-06 23:48:24 +0800

    Remove unnecessary dependency check

--- Assets/scripts/Controller/ChallengeLeaderboardController.cs
+++ Assets/scripts/Controller/ChallengeLeaderboardController.cs
@@ -17,34 +17,12 @@ public class ChallengeLeaderboardController : MonoBehaviour
 
     private List<ChallengeEntry> challengeEntries;
 
-    void Awake()
-    {
-
-    }
-
     void OnEnable()
     {
-        Debug.Log("Initiating Challenge LB Controller...");
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available)
-            {
-                FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://teamrubberduck-1420e.firebaseio.com/");
-                reference = FirebaseDatabase.DefaultInstance.RootReference;
-                retrieveChallengeFromDB();
-
-            }
-            else
-            {
-                UnityEngine.Debug.LogError(System.String.Format(
-                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
-            }
-        });
-
         positions.text = "";
         playerNames.text = "";
         scores.text = "";
+        retrieveChallengeFromDB();
     }
 
     private void retrieveChallengeFromDB()
